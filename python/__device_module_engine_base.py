d4b0344b980e0bcd8ab2bba136fbac90d2086102
Abolfazl <adanayidet@gmail.com>
2023-04-27 09:15:16 +0330

    update

--- python/__device_module_engine_base.py
+++ python/__device_module_engine_base.py
@@ -7,15 +7,15 @@ from .__UTAEndpoint import UTAEndpoint
 import os
 
 class _DeviceModuleEngineBase:
-    def __init__(self, uid: str, iotis_gate_url: str):
-        self.__uid = uid
+    def __init__(self, iotis_gate_url: str):
         self.__iotis_gate_url = iotis_gate_url
         self.__mode = None
     
-    def auth_by_device(self, node_key_file_path: str):
+    def auth_by_device(self, node_uid: str, node_key_file_path: str):
+        self.__node_uid = node_uid
         with open(node_key_file_path, 'r') as file:
             pkey = file.read()
-        self.__uta = UTAEndpoint(self.__uid, pkey)
+        self.__uta = UTAEndpoint(self.__node_uid, pkey)
         self.__mode = 'd'
 
     def auth_by_auser(self, username: str, auser_key_file_path: str):
@@ -25,14 +25,21 @@ class _DeviceModuleEngineBase:
         self.__uta = UTAEndpoint(self.__auser, pkey)
         self.__mode = 'a'
 
-    def pingPong(self, ftype: str, args: dict) -> Response:
+    def pingPong(self, ftype: str, args: dict, uid: str = None) -> Response:
+        target_uid = None
+        if uid is not None:
+            target_uid = uid
+        else:
+            target_uid = self.__node_uid
+        if target_uid is None:
+            raise Exception('UID (specific or node) is not set')
         if self.__mode is None:
             raise Exception('First call one of auth_... methods to initialize')
         signature = self.__uta.sign()
         req = {
             'type': ftype,
             'args': args,
-            'uid': self.__uid,
+            'uid':target_uid,
             'signature': signature
         }
         if self.__mode == 'a':
--- python/__neoiot.py
+++ python/__neoiot.py
@@ -11,17 +11,17 @@ class NEOIOTEngine(_DeviceModuleEngineBase):
         _DeviceModuleEngineBase.__init__(
             self, uid, iotis_gate_url)
 
-    def setup(self, pixels_count: int = 8) -> Response:
+    def setup(self, pixels_count: int = 8, uid: str = None) -> Response:
         """Use this function to use the neopixel controller.
 Please note that the neo must be connected to it's special dedicated pin (pin number 4) and after calling
 this function the pin can not be used until device is reset."""
-        return self.pingPong('neo_setup', {'count': pixels_count})
+        return self.pingPong('neo_setup', {'count': pixels_count}, uid)
     
-    def fill(self, red: int, green: int, blue: int) -> Response:
+    def fill(self, red: int, green: int, blue: int, uid: str = None) -> Response:
         """Use this function to fill the neo.
 "neo_setup" must be called before using this function."""
         for item in (red, green, blue):
             item = int(item)
             if item < 0 or item > 255:
                 raise Exception("Neo pixel rgb values must be between 0 and 255")
-        return self.pingPong("neo_fill", {"color": (red, green, blue)})
+        return self.pingPong("neo_fill", {"color": (red, green, blue)}, uid)
